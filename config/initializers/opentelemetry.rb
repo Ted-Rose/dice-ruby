e383060b0392044ff8dc4e8c097ecc3296add28c
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-06-27 16:55:49 +0300

    Hook in the Sentry span processor and propagator

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -4,4 +4,7 @@ require 'opentelemetry-exporter-otlp'
 OpenTelemetry::SDK.configure do |c|
   c.service_name = 'dice-ruby'
   c.use_all() # enables all instrumentation!
+  c.add_span_processor(Sentry::OpenTelemetry::SpanProcessor.instance)
 end
+
+OpenTelemetry.propagation = Sentry::OpenTelemetry::Propagator.new
